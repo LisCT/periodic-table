1b4b0f61afd97c4ea3706201632ae98c6d2bdafa
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-12-11 13:37:39 -0600

    adding deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "./logo.svg";
 
 const App: React.FC = () => (
   <Wrapper>
     <h1>Hello World</h1>
-    <img src={logo} className="App-logo" alt="logo" />
+    <img src="../images/logo.svg" className="App-logo" alt="logo" />
   </Wrapper>
 );
 
