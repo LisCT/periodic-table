4b2e35b37cda49e25172eb0dcbbf50a3fa3bac13
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-12-11 10:49:51 -0600

    adding typescript & prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
-const path = require('path');
+const path = require("path");
 
-const config = {
-  entry: ['./src/Index.js'],
+module.exports = {
+  entry: ["./src/Index.tsx"],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/public/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/public/",
+    filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, "public")
   },
+  devtool: "source-map",
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'env', 'stage-2']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["react", "env", "stage-2"]
+          }
         }
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
       }
-    }]
-  },
+    ]
+  }
 };
-
-module.exports = config;
