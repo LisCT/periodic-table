f0b0100651ef461444d525f5574769a37a622fcd
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-12-17 15:30:05 -0600

    documenting hooks

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 /**
  * Feching Data
- * * The generic data is getting from an API.
- * @param Api this method is expecting an URL.
+ * * The data is getting from an API via fetch().
+ * @param {string} Api - URL to be used for the useFetch(Api).
+ * @returns {array} - [data, isFetching]
  */
 
-const useFetch = (Api: string) => {
+const useFetch = (Api: string): Array<any> => {
   const [data, setData] = useState();
   const [isFetching, setIsFetching] = useState(true);
 
   const fetchingData = async () => {
     try {
       //console.log("1️⃣ - Start to fetch data from Api 🚚...");
-      const response = await axios.get(Api);
-      const data = await response.data;
+      const response = await fetch(Api);
+      const data = await response.json();
 
       //console.log("2️⃣ - Fetching data completed  ✅.");
       setData(data);
